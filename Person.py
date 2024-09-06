7a7c41d43b397c3240b9503d68c49c208d153a03
reddymaestro12 <reddymaestro12@gmail.com>
2024-09-06 12:13:41 +0530

    sal is added

--- Person.py
+++ Person.py
@@ -2,13 +2,14 @@ from datetime import date
 
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age,sal):
         self.name = name
         self.age = age
+        self.sal = sal
 
     # a class method to create a Person object by birth year.
     @classmethod
-    def fromBirthYear(cls, name, year):
+    def fromBirthYear(cls, name, year,sal):
         return cls(name, date.today().year - year)
 
     # a static method to check if a Person is adult or not.
@@ -17,8 +18,8 @@ class Person:
         return age > 18
 
 
-person1 = Person('mayank', 21)
-person2 = Person.fromBirthYear('mayank', 1996)
+person1 = Person('mayank', 21,1000)
+person2 = Person.fromBirthYear('mayank', 1996,2000)
 
 print(person1.age)
 print(person2.age)
